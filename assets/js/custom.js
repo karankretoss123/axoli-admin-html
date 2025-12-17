f4d6920d02d31be6861c4f8e0633ffde87a11e46
ruchikretoss <ruchi.j@kretoss.in>
2025-12-17 18:57:23 +0530

    logout change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -187,7 +187,7 @@ function toggleSidebar() {
  ***********************/
 function handleLogout() {
   localStorage.clear();
-  window.location.href = "login.html";
+  window.location.href = "index.html";
 }
 
 /***********************
